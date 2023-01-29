bc4326b2846376b73946fc07a881428165a72cfd
Adam Argyle <atom@argyleink.com>
2023-01-29 15:17:45 -0800

    hint styles

--- src/components/Gradient.svelte
+++ src/components/Gradient.svelte
@@ -90,8 +90,8 @@
 
   <!-- todo: multiple gradients -->
 
-  <div class="controls">
-    <fieldset style="accent-color: {$gradient_stops[0].color}">
+  <div class="controls" style="accent-color: {$gradient_stops[0].color}">
+    <fieldset>
       <legend>Type</legend>
       {#each gradient_types as t}
         <div class="type-switch">
@@ -190,9 +190,9 @@
     {/if}
 
     <!-- color stops -->
-    {#each $gradient_stops as stop}
+    {#each $gradient_stops as stop, i}
       {#if stop.kind === 'stop'}
-        <fieldset>
+        <fieldset style="accent-color: {stop.color}">
           <legend>Color</legend>
           <div class="chip color-stop">
             <input class="round" type="color" bind:value="{stop.color}">
@@ -207,10 +207,11 @@
         </fieldset>
       {/if}
       {#if stop.kind === 'hint'}
+        <!--  style="accent-color: {$gradient_stops[i-1].color}" -->
         <fieldset>
           <legend>Easing</legend>
           <div class="color-hint">
-            <input type="range" bind:value="{stop.percentage}">
+            <input type="range" bind:value="{stop.percentage}" style="background: linear-gradient(to right in {$gradient_space}, {$gradient_stops[i-1].color}, {$gradient_stops[i+1].color})">
           </div>
         </fieldset>
       {/if}
@@ -277,5 +278,29 @@
 
   input[type="color"].round::-webkit-color-swatch {
     border: none;
+  }
+
+  .color-hint > input {
+    appearance: none;
+    accent-color: var(--surface-1);
+    border-radius: var(--radius-round);
+    block-size: 1rem;
+  }
+
+  .color-hint > input::-webkit-slider-thumb {
+    --_border-size: 4px;
+    
+    cursor: grab;
+    appearance: none;
+    accent-color: var(--surface-1);
+    border: 4px solid white;
+    height: calc(1rem + (var(--_border-size) * 2));
+    aspect-ratio: 1;
+    border-radius: var(--radius-round);
+    box-shadow: var(--shadow-2), var(--inner-shadow-2);
+  }
+  
+  .color-hint > input:active::-webkit-slider-thumb {
+    cursor: grabbing;
   }
 </style>
