ab79b0ec4ed9f9959fce38ee2fb692906e9dfed3
Adam Argyle <argyle@google.com>
2023-02-03 10:40:47 -0800

    fade in after prism runs

--- src/components/PrismJS.svelte
+++ src/components/PrismJS.svelte
@@ -4,18 +4,27 @@
   
   export let language;
   export let code;
+  let loaded = false
   
   onMount(() => {
+    loaded = true
     Prism.highlightAll();
   });
 </script>
 
-<pre class="code-block">
+<pre class="code-block" has-loaded={loaded}>
   <code class="language-{language}">{code}</code>
 </pre>
 
 <style>
   .code-block {
+    opacity: 0;
+    max-inline-size: var(--size-content-3);
+    justify-self: center;
     background-color: var(--gray-10);
+  }
+
+  .code-block[has-loaded="true"] {
+    animation: var(--animation-fade-in) forwards;
   }
 </style>
