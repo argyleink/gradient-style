8269178914948291cb3cc6fb56ec5d59988e2ee1
Adam Argyle <atom@argyleink.com>
2023-02-03 20:37:28 -0800

    add  num to angle sliders

--- src/components/ConicAngle.svelte
+++ src/components/ConicAngle.svelte
@@ -8,8 +8,6 @@
   <div class="linear-angle slider-set">
     <input type="range" bind:value={$conic_angle} min="0" max="360" step="1" />
     <AngleIcon angle={$conic_angle} />
-    {#if $conic_angle != null}
-      <button class="remove container-absolute" type="reset" on:click={() => $conic_angle = 0}>✕</button>
-    {/if}
+    <input type="number" bind:value={$conic_angle} min="0" max="360" step="1"  class="slider-percentage">
   </div>
 </fieldset>
--- src/components/LinearAngle.svelte
+++ src/components/LinearAngle.svelte
@@ -24,8 +24,6 @@
   <div class="linear-angle slider-set">
     <input type="range" bind:value={$linear_angle} min="0" max="360" step="1" style="accent-color: {$linear_angle === null ? 'var(--gray-5)' : 'inherit'}" />
     <AngleIcon angle={$linear_angle} />
-    {#if $linear_angle != null}
-      <button class="remove container-absolute" type="reset" on:click={() => $linear_angle = null}>✕</button>
-    {/if}
+    <input type="number" bind:value={$linear_angle} min="0" max="360" step="1"  class="slider-percentage">
   </div>
 </fieldset>
