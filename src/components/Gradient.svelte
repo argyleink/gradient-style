defa37a1db29d0afbd1d3375a37f685562d258b6
Adam Argyle <argyle@google.com>
2023-03-31 11:23:44 -0700

    adds colorspace info button

--- src/components/Gradient.svelte
+++ src/components/Gradient.svelte
@@ -294,7 +294,7 @@
   <contain-er style="container: control-panel / inline-size; z-index: var(--layer-1)">
     <section class="controls">
       <div class="menu-bar">
-        <button class="settings" disabled>
+        <button class="menu-bar-icon" disabled>
           <span class="sr-only">Settings</span>
           <svg viewBox="0 0 24 24" aria-hidden="true">
             <path fill="currentColor" d="m9.25 22l-.4-3.2q-.325-.125-.613-.3t-.562-.375L4.7 19.375l-2.75-4.75l2.575-1.95Q4.5 12.5 4.5 12.337v-.674q0-.163.025-.338L1.95 9.375l2.75-4.75l2.975 1.25q.275-.2.575-.375t.6-.3l.4-3.2h5.5l.4 3.2q.325.125.613.3t.562.375l2.975-1.25l2.75 4.75l-2.575 1.95q.025.175.025.338v.674q0 .163-.05.338l2.575 1.95l-2.75 4.75l-2.95-1.25q-.275.2-.575.375t-.6.3l-.4 3.2h-5.5Zm2.8-6.5q1.45 0 2.475-1.025T15.55 12q0-1.45-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12q0 1.45 1.012 2.475T12.05 15.5Z"/>
@@ -394,7 +394,7 @@
     justify-content: end;
     align-items: center;
     gap: var(--size-3);
-    padding-inline: var(--size-3);
+    padding-inline: var(--size-4);
     position: sticky;
     z-index: 1;
     inset-block-start: 0;
@@ -414,11 +414,11 @@
     box-shadow: none;
   }
 
-  .menu-bar > .settings {
+  .menu-bar-icon {
     padding: var(--size-1);
     border-radius: var(--radius-round);
     aspect-ratio: var(--ratio-square);
-/*     box-shadow: 0 0 0 var(--_highlight-size) var(--_highlight); */
+    box-shadow: 0 0 0 var(--_highlight-size) var(--_highlight);
 
     --_bg: transparent;
     --_icon-size: var(--size-4);
