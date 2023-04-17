286cb10fea2a9e986b2275f10e073ed7cf8124d8
Adam Argyle <atom@argyleink.com>
2023-04-16 21:59:29 -0700

    prep for meta theme updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,8 @@
 
 <svelte:head>
 	<title>CSS HD Gradients</title>
-	<meta name="description" content="Color 4 complaint CSS gradient builder." />
+	<meta name="description" content="Wide gamut Color 4 complaint CSS gradient builder." />
+	<meta id="browsertheme" name="theme-color" content="#ff00ff">
 </svelte:head>
 
 <Gradient />
