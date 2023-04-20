3c3d2d656455fa21cada6029b6954c293d964887
Adam Argyle <atom@argyleink.com>
2023-04-19 22:33:40 -0700

    hint copy fixes

--- src/components/Gradient.svelte
+++ src/components/Gradient.svelte
@@ -43,7 +43,7 @@
   let svgicon
 
   onMount(async () => {
-    preview_hd = window.matchMedia('(dynamic-range: high)').matches
+    // preview_hd = window.matchMedia('(dynamic-range: high)').matches
 
     const {stateAsString, restoreStateFromUrl} = await import('../store/url.ts')
     const restore = restoreStateFromUrl()
@@ -319,7 +319,7 @@
         </div>
         <div class="preview">
           <label class="hd-switch">
-            <Hint title="HDR" copy="This switch is" />
+            <Hint title="HDR" copy="This switch toggles between the SDR and HDR CSS gradients.<br><br>When off it shows the gradient with hex colors, which has always been in the sRGB colorspace.<br><br>When on, the gradient shown is using a newer color space and also using newer wide gamut colors.<br><br>This button helps you preview the classic gradient vs the HD gradient you're building in this tool." />
             <span class="sr-only">HD on or off?</span>
             {#if preview_hd == true}
               <svg aria-hidden="true" width="32" height="32" viewBox="0 0 20 20"><path fill="currentColor" d="M4.75 4A2.75 2.75 0 0 0 2 6.75v6.5A2.75 2.75 0 0 0 4.75 16h10.5A2.75 2.75 0 0 0 18 13.25v-6.5A2.75 2.75 0 0 0 15.25 4H4.75ZM4.5 7.5A.5.5 0 0 1 5 8v1.5h2V8a.5.5 0 0 1 1 0v4a.5.5 0 0 1-1 0v-1.5H5V12a.5.5 0 0 1-1 0V8a.5.5 0 0 1 .5-.5ZM9 8a.5.5 0 0 1 .5-.5h.25A2.25 2.25 0 0 1 12 9.75v.5a2.25 2.25 0 0 1-2.25 2.25H9.5A.5.5 0 0 1 9 12V8Zm1 3.475c.57-.116 1-.62 1-1.225v-.5c0-.605-.43-1.11-1-1.225v2.95ZM13 8a.5.5 0 0 1 .5-.5h1a1.5 1.5 0 0 1 .868 2.724l.6 1.6a.5.5 0 0 1-.936.352l-.629-1.676H14V12a.5.5 0 0 1-1 0V8Zm1 1.5h.5a.5.5 0 0 0 0-1H14v1Z"/></svg>
